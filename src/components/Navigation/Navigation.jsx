65b7072af4cbcee61d02dc84d4d72d1e31907351
Aleksey Konstantinov Ubunty <alex_kav@mail.ru>
2023-02-23 12:12:17 +0300

    feat: add redusers category createAsyncThunk

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,12 +2,19 @@ import { Container } from '../Container/Container';
 import style from './Navigation.module.css';
 import classNames from 'classnames';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeCategory } from '../../store/category/categorySlice';
+import { categoryRequestAsync, changeCategory } from '../../store/category/categorySlice';
+import { useEffect } from 'react';
+import { API_URI } from '../../const';
 
 export const Navigation = () => {
   const { category, activeCategory } = useSelector((state) => state.category);
-  const dispatch = useDispatch();
+  const dispatch = useDispatch();  
 
+  useEffect(
+    () => {
+      dispatch(categoryRequestAsync());
+    }, []);
+  
   return (
     <nav className={style.navigation}>
       <Container className={style.navigation__container}>
@@ -18,7 +25,7 @@ export const Navigation = () => {
                 className={classNames(
                   style.navigation__button,
                   activeCategory === i ? style.navigation__button_active : '')}                  
-                style={{ backgroundImage: `url(${item.image})` }}
+                style={{ backgroundImage: `url(${API_URI}/${item.image})` }}
                 onClick={() => {
                   dispatch(changeCategory({ indexCategory: i }));
                 }}
--- src/const.js
+++ src/const.js
@@ -1,1 +1,2 @@
-export const URL = 'http://localhost:3024';
+export const API_URI = 'http://localhost:3024';
+export const POSTFIX = '/api/product';
