3f3a4ac2a2fde901aa1982056ec11d9ccb713b76
Aleksey Konstantinov Ubunty <alex_kav@mail.ru>
2023-02-25 13:37:27 +0300

    update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,7 +13,7 @@ export const Footer = () => (
           <div className={style.footer__contact}>
             <h2 className={style.footer__title}>Номер для заказа</h2>
 
-            <a className={style.footer__phone} href='tel:+79308333811'>
+            <a className={style.footer__phone} href='tel:+79999999999'>
               <svg
                 width='24'
                 height='24'
@@ -23,7 +23,7 @@ export const Footer = () => (
                 <path d='M20.01 15.38C18.78 15.38 17.59 15.18 16.48 14.82C16.13 14.7 15.74 14.79 15.47 15.06L13.9 17.03C11.07 15.68 8.42 13.13 7.01 10.2L8.96 8.54C9.23 8.26 9.31 7.87 9.2 7.52C8.83 6.41 8.64 5.22 8.64 3.99C8.64 3.45 8.19 3 7.65 3H4.19C3.65 3 3 3.24 3 3.99C3 13.28 10.73 21 20.01 21C20.72 21 21 20.37 21 19.82V16.37C21 15.83 20.55 15.38 20.01 15.38Z' />
               </svg>
 
-              <span>+7(930)833-38-11</span>
+              <span>+7(999)999-99-99</span>
             </a>
           </div>
 
@@ -67,7 +67,7 @@ export const Footer = () => (
           </p>
           <p>
             Developer:{' '}
-            <a href='mailto:maksim.leskin@methed.ru'>Maksim Leskin</a>
+            <a href='mailto:info@alekseykonstantinov.ru'>Aleksey Konstantinov</a>
           </p>
         </div>
       </div>
