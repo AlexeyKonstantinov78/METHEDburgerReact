bb454795bcc787b79e1abdedad8ec5dc906b455a
Aleksey Konstantinov Ubunty <alex_kav@mail.ru>
2023-02-25 20:37:10 +0300

    update const

--- src/const.js
+++ src/const.js
@@ -1,3 +1,4 @@
-export const API_URI = 'http://localhost:3024';
+export const API_URI = 'https://api-your-meal.glitch.me';
+// export const API_URI = 'http://localhost:3024';
 export const POSTFIX = '/api/product';
 export const API_URI_SUBMIT = 'https://cloudy-slash-rubidium.glitch.me/api/order';
